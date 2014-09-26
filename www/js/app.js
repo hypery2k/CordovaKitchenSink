0e58727badedf9e1961b1d9f3a4eb18514c01e74
Christian Speckner <christian.speckner@mayflower.de>
2014-09-26 12:21:34 +0200

    Block until cordova has loaded, close page menu after navigation.

--- www/js/app.js
+++ www/js/app.js
@@ -17,8 +17,11 @@
  * under the License.
  */
 var app = {
+    pageApi: null,
+
     // Application Constructor
-    initialize: function() {
+    initialize: function(pageApi) {
+        this.pageApi = pageApi;
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -26,26 +29,13 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', _.bind(this.onDeviceReady, this), false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        this.pageApi.hideLoader();
     }
 };
-
-app.initialize();
